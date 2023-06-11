5de82514e7125af2cd32d23224de8a293a811b79
Phương <20521779@gm.uit.edu.vn>
2023-06-11 22:52:32 +0700

    Revert "Merge branch 'main' into MyPhuong"
    This reverts commit 8a84e991d618fc01f192a366863c0d68c536f9c4, reversing
    changes made to d0ecb985206b01ec7f9ee538538ac761a5cde3d6.

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import AppContainer from "./Navigation/AppNavigator";
 import AppNavigator from "./Navigation/AppNavigator";
-import { UserProvider } from "./contextObject";
 
 export default function App() {
-  return (
-    <UserProvider>
-      <AppNavigator></AppNavigator>
-    </UserProvider>
-  );
+  return <AppNavigator></AppNavigator>;
 }
